bfdb9a52e750a295823f04b6f4fe76f02a81158d
stochastic-sisyphus <102266523+stochastic-sisyphus@users.noreply.github.com>
2024-10-22 13:47:39 -0500

    Delete adv_data_processing_pipeline directory

--- adv_data_processing_pipeline/feature_selection.py
+++
@@ -1,9 +0,0 @@
-from sklearn.feature_selection import SelectKBest, f_classif
-
-def select_features(X: pd.DataFrame, y: pd.Series, k: int = 10) -> List[str]:
-    """Select the top k features using ANOVA F-value."""
-    selector = SelectKBest(score_func=f_classif, k=k)
-    selector.fit(X, y)
-    selected_features = X.columns[selector.get_support()].tolist()
-    return selected_features
-
